3c8de6224a2dde0fa7b5c4e245c77b7526100e74
Jas Badhan <jas.badhan@torontomu.ca>
2025-10-03 01:01:40 -0400

    Update labs109.py

--- labs109.py
+++ labs109.py
@@ -1,3 +1,5 @@
+n = 100
+
 def ryerson_letter_grade(n):
     if n < 50:
         return 'F'
@@ -16,3 +18,36 @@ def ryerson_letter_grade(n):
     else:
         adjust = ""
     return "DCB"[tens - 5] + adjust
+
+def is_ascending(items):
+    pre_item = items[0] - 1
+    for item in items:
+        if item <= pre_item:
+            return False
+        pre_item = item
+    return True
+    
+def only_odd_digits(n):
+    total_length = len(str(n))
+    len_n = 0
+    for digit in str(n):
+        if int(digit) % 2 != 0:
+            len_n += 1
+    if len_n == total_length:
+        return True
+    else:
+        return False
+
+def riffle(items, out = True):
+    items_left = items[:len(items)//2]
+    items_right = items[len(items)//2:]
+    items_riffle = []
+    if out:
+        for i in range(len(items_left)):
+            items_riffle.append(items_left[i])
+            items_riffle.append(items_right[i])
+    else:
+        for i in range(len(items_right)):
+            items_riffle.append(items_right[i])
+            items_riffle.append(items_left[i])
+    return items_riffle
